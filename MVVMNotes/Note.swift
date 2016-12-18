7daf98d28bdaa4d689f24f2ee975c17c2e1ad2e6
Evan Grossman <grossmae@gmail.com>
2016-12-17 18:35:02 -0800

    Add Note model

---
+++ MVVMNotes/Note.swift
@@ -0,0 +1,16 @@
+//
+//  Note.swift
+//  MVVMNotes
+//
+//  Created by Evan Grossman on 12/17/16.
+//  Copyright © 2016 Evan Grossman. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    let id: String
+    let created: Date
+    let title: String
+    let content: String
+}
