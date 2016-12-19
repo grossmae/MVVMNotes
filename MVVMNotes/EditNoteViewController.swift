56deaf218866209e941f53b2a4bdb6935bcf5db3
Evan Grossman <grossmae@gmail.com>
2016-12-19 15:42:14 -0800

    Add UI for entering note title and content in the EditNoteViewController

--- MVVMNotes/EditNoteViewController.swift
+++ MVVMNotes/EditNoteViewController.swift
@@ -25,6 +25,23 @@ class EditNoteViewController: UIViewController {
         return doneBarButtonItem
     }()
     
+    let titleTextField: UITextField = {
+        let titleTextField = UITextField()
+        titleTextField.placeholder = "Title"
+        return titleTextField
+    }()
+    
+    let contentLabel: UILabel = {
+        let contentLabel = UILabel()
+        contentLabel.text = "Content:"
+        return contentLabel
+    }()
+    
+    let contentTextView: UITextView = {
+        let contentTextView = UITextView()
+        return contentTextView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -45,6 +62,30 @@ class EditNoteViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = cancelBarButtonItem
         navigationItem.rightBarButtonItem = doneBarButtonItem
+        
+        view.addSubview(titleTextField)
+        titleTextField.snp.makeConstraints { (make) in
+            make.top.equalTo(topLayoutGuide.snp.bottom).offset(7)
+            make.left.equalTo(7)
+            make.right.equalTo(-7)
+            make.height.equalTo(30)
+        }
+        
+        view.addSubview(contentLabel)
+        contentLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(titleTextField.snp.bottom).offset(7)
+            make.left.equalTo(7)
+            make.right.equalTo(7)
+            make.height.equalTo(25)
+        }
+        
+        view.addSubview(contentTextView)
+        contentTextView.snp.makeConstraints { (make) in
+            make.top.equalTo(contentLabel.snp.bottom).offset(7)
+            make.left.equalTo(7)
+            make.right.equalTo(-7)
+            make.bottom.equalTo(bottomLayoutGuide.snp.top).offset(7)
+        }
     }
     
     func cancelPressed() {
